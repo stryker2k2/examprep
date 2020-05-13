4d176c82c0dba3a2d7f234353357cd2e2dfcaa9e
stryker2k2 <stryker2k2@msn.com>
2020-05-13 09:12:45 -0700

    Update getK32.asm

--- journeyman/getK32.asm
+++ journeyman/getK32.asm
@@ -1,1 +1,30 @@
-test
+;;;;;;;;;;;;;;;;;;;; 
+;                  ; 
+; WORK IN PROGRESS ;
+;                  ;
+;;;;;;;;;;;;;;;;;;;;
+
+global _main
+
+section .data
+section .bss
+section .text
+_main:
+	mov ebx, fs:0x30	; Get pointer to PEB
+	mov ebx, [ebx + 0x0C] ; Get pointer to PEB_LDR_DATA
+	mov ebx, [ebx + 0x14] ; Get pointer to first entry in InMemoryOrderModuleList
+	mov ebx, [ebx]		; Get pointer to second (ntdll.dll) entry in InMemoryOrderModuleList
+	mov ebx, [ebx]		; Get pointer to third (kernel32.dll) entry in InMemoryOrderModuleList
+	mov ebx, [ebx + 0x10] ; Get kernel32.dll base address
+ret
+
+; TO COMPILE:
+; del *.obj && del *.o && del *.exe
+;
+; //WIN32
+; nasm -f win32 helloworld.asm
+; ld helloworld.obj -o HelloWorld.exe
+;
+; //ELF
+; nasm -f elf helloworld.asm
+; gcc helloworld.o -o HelloWorld.exe
